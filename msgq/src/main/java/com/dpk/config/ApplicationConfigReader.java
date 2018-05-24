44a7ba2443da8468cc22850d33aee0445d260b7c
Deepak Kumar <deepak4ev@gmail.com>
2018-05-24 11:19:20 +0530

    Updated ApplicationConfigReader file

--- msgq/src/main/java/com/dpk/config/ApplicationConfigReader.java
+++ msgq/src/main/java/com/dpk/config/ApplicationConfigReader.java
@@ -11,7 +11,7 @@ public class ApplicationConfigReader {
 	@Value("${app1.exchange.name}")
 	private String app1Exchange;
 	
-	@Value("$app1.queue.name}")
+	@Value("${app1.queue.name}")
 	private String app1Queue;
 	
 	@Value("${app1.routing.key}")
@@ -20,7 +20,7 @@ public class ApplicationConfigReader {
 	@Value("${app2.exchange.name}")
 	private String app2Exchange;
 	
-	@Value("$app2.queue.name}")
+	@Value("${app2.queue.name}")
 	private String app2Queue;
 	
 	@Value("${app2.routing.key}")
